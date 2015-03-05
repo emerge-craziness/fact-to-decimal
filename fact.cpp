ca0796fe5678e386f147d2aa85858cc91b5b3ecb
emerge-craziness <ispikulin@yandex.ru>
2015-03-05 22:09:18 +0300

    fact.cpp became a bit more good

--- fact.cpp
+++ fact.cpp
@@ -4,32 +4,33 @@ using namespace std;
 
 class Methods {
 	    private:
-		    long int fact(int number) {
-		        long int r = 1;
+		    long long int fact(int number) {
+		        long long int r = 1;
 		        for(int i = 1; i <= number; ++i) {
-		            r *= (long int)i;
+		            r *= (long long int)i;
 		        }   
 	        return r;
-		    }  
+        }  
 
         public:
-			long int f2d() {
+			long long int f2d() {
 				string number_string;
+                cout << "Enter your number (letters must be uppercase): ";
 				cin >> number_string;
 				int number[number_string.size()];
 				for(int i = 0; i < number_string.size(); ++i) {
                     if(isdigit(number_string[i])) number[i] = (int)number_string[i] - 48;
                     else number[i] = (int)number_string[i] - 55;
-                            //HEY! 'A' & 'a' has different ANCII codes!
-                            //I used uppercase 'A' here.
+                            //'A' & 'a' have different ANCII codes!
+                            //I've used the uppercase 'A' here.
                 }
 
                 for(int i = 1; i <= number_string.size(); ++i) {
                     int t = number_string.size() - i;
-                    if (number[t] > fact(i)) return 666;
+                    if (number[t] > fact(i)) return -1;
                 }
 
-                long int result = 0;
+                long long int result = 0;
                 for(int i = 1; i <= number_string.size(); ++i) {
                     int t = number_string.size() - i;
                     result += number[t] * fact(i);
@@ -38,7 +39,8 @@ class Methods {
             }
 
             int d2f() {
-                long int number;
+                long long int number;
+                cout << "Enter your number: ";
                 cin >> number;
 
                 if(number == 0) {
@@ -73,12 +75,28 @@ class Methods {
 
 int main(int argc, char * argv[]) {
     if(argc < 2) {
-        cout << "You had to input \'f\' or \'d\' literal to convert your number to factorial or decimal scale of notation in accordance. (For example: " << argv[0] << " f)\n" << "Note: if you input invalid number in fact.c.n., the program returns \'666\' error.\n";
+        cout << "Possible arguments:\n" <<
+                 "-f translates the number into the factorial num system\n" <<
+                 "-d translates it to the decimal\n\n" << 
+                 "If your number is incorrect, program returns -1.\n";
         return 1;
     }
 
     Methods f;
-    if(argv[1][0] == 'f') f.d2f();
-    else if(argv[1][0] == 'd') cout << f.f2d() << '\n';
-    else cout << "Incorrect literal\n";
+    if (argv[1][0] == '-') {
+        switch (argv[1][1]) {
+        case 'f':
+            cout << f.d2f() << "\n";
+            break;
+        case 'd': 
+            cout << f.f2d() << "\n";
+            break;
+        default:
+            cout << "Incorrect argument\n";
+            break;
+        }
+    }
+    else {
+        cout << "Incorrect argument\n";
+    }
 }
